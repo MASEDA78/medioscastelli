9ce5892ebfbc58f22f9ee805f3bbfb4703ba5579
sdmdesarrollo <sistemassdm78@gmail.com>
2025-09-06 10:05:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ function validar() {
   const clave = document.getElementById("clave").value.trim();
 
   if (usuario === "medioscastelli" && clave === "medios2025") {
-    window.location.href = "inicio.html";
+    document.getElementById("loginView").classList.add("hidden");
+    document.getElementById("inicioView").classList.remove("hidden");
   } else {
     document.getElementById("error").innerText = "Usuario o clave incorrectos.";
   }
+}
+
+function mostrarMedios() {
+  document.getElementById("inicioView").classList.add("hidden");
+  document.getElementById("mediosView").classList.remove("hidden");
+}
+
+function volverInicio() {
+  document.getElementById("mediosView").classList.add("hidden");
+  document.getElementById("inicioView").classList.remove("hidden");
 }
